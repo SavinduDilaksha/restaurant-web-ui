83c5ff89b5b025f2cc85fe1ebf12451e75efa1a5
savindu.dilaksha147@gmail.com <savindu.dilaksha147@gmail.com>
2026-01-20 20:21:14 +0530

    Add order system page structure (services, cart, checkout, confirmation)

---
+++ restaurant-web-ui/src/pages/Order/Confirmation.jsx
@@ -0,0 +1,5 @@
+function Confirmation() {
+  return <h2>Order Confirmation Page</h2>;
+}
+
+export default Confirmation;
---
+++ restaurant-web-ui/src/pages/Order/Services.jsx
@@ -0,0 +1,5 @@
+function Services() {
+  return <h2>Online Ordering Services Page</h2>;
+}
+
+export default Services;
